fa2747af46fb52bf2681934f1868552a2e2b3bfe
Nathan Godfrey <nathan.godfrey@tpximpact.com>
2022-08-17 15:52:05 +0100

    Added app hostname output

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ resource "azurerm_resource_group" "rg" {
   location = "uksouth"
 }
 
-module az_app {
-  source = "./modules/az_app"
-  rg_name = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
+module "az_app" {
+  source            = "./modules/az_app"
+  rg_name           = azurerm_resource_group.rg.name
+  location          = azurerm_resource_group.rg.location
   service_plan_name = "school-demo"
-  web_app_name = "school-demo"
+  web_app_name      = "school-demo"
 }
